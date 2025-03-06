7dedb8b1b36efb12f9fc99517f3a6e59715e6b0c
maksymads <147048960+maksymads@users.noreply.github.com>
2025-03-06 10:02:49 +0100

    Update baza.sql

--- baza.sql
+++ baza.sql
@@ -6,5 +6,16 @@ CREATE TABLE tasks (
     id INT AUTO_INCREMENT PRIMARY KEY,
     task VARCHAR(255) NOT NULL,
     category VARCHAR(50),
-    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+    priority ENUM('Niski', 'Średni', 'Wysoki') DEFAULT 'Średni',
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    planned_end_date DATETIME,
+    completed BOOLEAN DEFAULT FALSE
+);
+
+CREATE TABLE comments (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    task_id INT NOT NULL,
+    comment TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    FOREIGN KEY (task_id) REFERENCES tasks(id) ON DELETE CASCADE
 );
